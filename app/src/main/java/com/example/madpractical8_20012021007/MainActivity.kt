7559732684544391fa6f9d2c47cf59463f0656b0
roshnipatell <patelroshni9045@gmail.com>
2022-10-03 12:40:16 +0530

    Practical - 8 completed

--- app/src/main/java/com/example/madpractical8_20012021007/MainActivity.kt
+++ app/src/main/java/com/example/madpractical8_20012021007/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.madpractical8_20012021007
 
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var alarm_img : ImageView
+    lateinit var heart_img : ImageView
+    lateinit var alarm_animation : AnimationDrawable
+    lateinit var heart_animation : AnimationDrawable
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        alarm_img = findViewById(R.id.alarm_list)
+        alarm_img.setBackgroundResource((R.drawable.alarm_list))
+        alarm_animation = alarm_img.background as AnimationDrawable
+        heart_img = findViewById(R.id.heart_list)
+        heart_img.setBackgroundResource((R.drawable.heart_list))
+        heart_animation = heart_img.background as AnimationDrawable
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if(hasFocus){
+            alarm_animation.start()
+            heart_animation.start()
+        }
+        else{
+            alarm_animation.stop()
+            heart_animation.stop()
+        }
     }
 }
